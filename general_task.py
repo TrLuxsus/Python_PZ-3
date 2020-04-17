638f1f8a0d063e8cc43f9ef831ba8dd80330e1c4
Dima Moruga <trluxsus@outlook.com>
2020-04-17 12:03:53 +0300

    Implement test script.

--- general_task.py
+++ general_task.py
@@ -2,3 +2,15 @@ class Worker:
     age = 0
     name = ""
     __weight = 0
+
+
+def main():
+    wrk1 = Worker ()
+    wrk1.age = 34
+    wrk1.name = "Иванов"
+
+    print(wrk1, wrk1.age, wrk1.name)
+
+    
+if __name__ == "__main__":
+    main()
