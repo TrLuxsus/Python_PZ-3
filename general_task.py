dba7d04a9d5ec4991d1796002d15e3d4b1282c94
Dima Moruga <trluxsus@outlook.com>
2020-04-17 12:05:59 +0300

    Implement eat, get_weight methods.

--- general_task.py
+++ general_task.py
@@ -3,6 +3,12 @@ class Worker:
     name = ""
     __weight = 0
 
+    def eat (self, how_much):
+        self.__weight += how_much
+
+    def get_weight(self):
+        return self.__weight
+
 
 def main():
     wrk1 = Worker ()
@@ -11,6 +17,6 @@ def main():
 
     print(wrk1, wrk1.age, wrk1.name)
 
-    
+
 if __name__ == "__main__":
     main()
